d0f81bd16bcd5b3e83d359d81548d92f0fcaf58f
CptMiracle <56608216+CptMiracle@users.noreply.github.com>
2021-06-03 14:33:16 -0400

    test

---
+++ js/script.js
@@ -0,0 +1,25 @@
+
+var testing = document.getElementById("button");
+    testing.addEventListener("click", function() { 
+        alert("test") });
+document.write("Hey");
+
+    
+
+
+// function clickedwhite() {
+//     alert("Clicked white");
+//   };
+
+//   function clickedblue() {
+//     alert("Clicked blue");
+//   };
+//   function clickedpurple() {
+//     alert("Clicked purple");
+//   };
+//   function clickedorange() {
+//     alert("Clicked orange");
+//   };
+//   function clickedgreen() {
+//     alert("Clicked green");
+//   };
