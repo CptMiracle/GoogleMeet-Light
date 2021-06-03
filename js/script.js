361226c481f1fe75d7049facc6fa3ddf4c28e9ef
CptMiracle <56608216+CptMiracle@users.noreply.github.com>
2021-06-03 14:50:34 -0400

    t

--- js/script.js
+++ js/script.js
@@ -1,25 +1,47 @@
 
-var testing = document.getElementById("button");
-    testing.addEventListener("click", function() { 
-        alert("test") });
-document.write("Hey");
+var white = document.getElementById("white");
+var blue = document.getElementById("blue");
+var purple = document.getElementById("purple");
+var orange = document.getElementById("orange");
+var green = document.getElementById("green");
 
-    
+white.addEventListener("click", function() { 
+    alert("Switched to white theme.") 
+    var elements = document.getElementsByClassName('p2ZbV'); // get all elements
+	for(var i = 0; i < elements.length; i++){
+		elements[i].style.background = "#ffffff !important";
+	}
+
+    var elements = document.getElementsByClassName('KWIIWd'); // get all elements
+	for(var i = 0; i < elements.length; i++){
+		elements[i].style.background = "#ffffff !important";
+	}
+
+    var elements = document.getElementsByClassName('p2ZbV'); // get all elements
+	for(var i = 0; i < elements.length; i++){
+		elements[i].style.background = "#ffffff !important";
+	}
+
+    var elements = document.getElementsByClassName('p2ZbV'); // get all elements
+	for(var i = 0; i < elements.length; i++){
+		elements[i].style.background = "#ffffff !important";
+	}
+});
+
+blue.addEventListener("click", function() { 
+    alert("Switched to blue theme.") 
+});
+
+purple.addEventListener("click", function() { 
+    alert("Switched to purple theme.") 
+});
+
+orange.addEventListener("click", function() { 
+    alert("Switched to orange theme.") 
+});
+
+green.addEventListener("click", function() { 
+    alert("Switched to green theme.") 
+});
 
 
-// function clickedwhite() {
-//     alert("Clicked white");
-//   };
-
-//   function clickedblue() {
-//     alert("Clicked blue");
-//   };
-//   function clickedpurple() {
-//     alert("Clicked purple");
-//   };
-//   function clickedorange() {
-//     alert("Clicked orange");
-//   };
-//   function clickedgreen() {
-//     alert("Clicked green");
-//   };
