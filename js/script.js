07d180e5c0ccaf2188958291d0005c103d4e6b3b
CptMiracle <56608216+CptMiracle@users.noreply.github.com>
2021-06-03 15:23:11 -0400

    test

--- js/script.js
+++ js/script.js
@@ -1,47 +1,55 @@
-
 var white = document.getElementById("white");
 var blue = document.getElementById("blue");
 var purple = document.getElementById("purple");
 var orange = document.getElementById("orange");
 var green = document.getElementById("green");
 
-white.addEventListener("click", function() { 
-    alert("Switched to white theme.") 
-    var elements = document.getElementsByClassName('p2ZbV'); // get all elements
-	for(var i = 0; i < elements.length; i++){
-		elements[i].style.background = "#ffffff !important";
-	}
+var list1 = [];
+var list2 = [];
+var list3 = [];
+var list4 = [];
 
-    var elements = document.getElementsByClassName('KWIIWd'); // get all elements
-	for(var i = 0; i < elements.length; i++){
-		elements[i].style.background = "#ffffff !important";
-	}
 
-    var elements = document.getElementsByClassName('p2ZbV'); // get all elements
-	for(var i = 0; i < elements.length; i++){
-		elements[i].style.background = "#ffffff !important";
-	}
+white.addEventListener("click", function () { //when white button is clicked
+  alert("Switched to white theme.");
+  var elements = document.getElementsByClassName("p2ZbV"); // get all elements
+  for (var i = 0; i < elements.length; i++) {
+    elements[i].style.background = "#ffffff !important";
+  }
 
-    var elements = document.getElementsByClassName('p2ZbV'); // get all elements
-	for(var i = 0; i < elements.length; i++){
-		elements[i].style.background = "#ffffff !important";
-	}
+  var elements = document.getElementsByClassName("KWIIWd"); // get all elements
+  for (var i = 0; i < elements.length; i++) {
+    elements[i].style.background = "#ffffff !important";
+  }
+
+  var elements = document.getElementsByClassName("xPh1xb P9KVBf"); // get all elements
+  for (var i = 0; i < elements.length; i++) {
+    elements[i].style.background = "#55585a !important";
+  }
+
+  var elements = document.getElementsByClassName("zWfAib:not(zTETae):not(.n9oEIb)>.Zf0RDc"); // get all elements
+  for (var i = 0; i < elements.length; i++) {
+    elements[i].style.background = "#808080 !important";
+  }
+
+
+  //GOOD NOW WE HAVE TO FIND A WAY TO STORE THE DATA
+
+//   localStorage.name = 
 });
 
-blue.addEventListener("click", function() { 
-    alert("Switched to blue theme.") 
+blue.addEventListener("click", function () {
+  alert("Switched to blue theme.");
 });
 
-purple.addEventListener("click", function() { 
-    alert("Switched to purple theme.") 
+purple.addEventListener("click", function () {
+  alert("Switched to purple theme.");
 });
 
-orange.addEventListener("click", function() { 
-    alert("Switched to orange theme.") 
+orange.addEventListener("click", function () {
+  alert("Switched to orange theme.");
 });
 
-green.addEventListener("click", function() { 
-    alert("Switched to green theme.") 
+green.addEventListener("click", function () {
+  alert("Switched to green theme.");
 });
-
-
