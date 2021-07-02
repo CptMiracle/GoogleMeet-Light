d493f36a5bdf5064a94c84e8523c1e101d22c154
CptMiracle <56608216+CptMiracle@users.noreply.github.com>
2021-07-01 21:38:30 -0400

    commit

--- js/script2.js
+++ js/script2.js
@@ -1,12 +1,17 @@
+var storage = chrome.storage.sync;
+
+chrome.tabs.insertCSS(null, {file: storage})
 
 white.addEventListener("click", function () {
 //   alert("Switched to white theme.");
     chrome.tabs.insertCSS(null, {file:"styles/style-white.css"})
+    chrome.storage.sync.set({file: "styles/style-white.css"})
 });
 
 blue.addEventListener("click", function () {
 //   alert("Switched to blue theme.");
     chrome.tabs.insertCSS(null, {file:"styles/style-blue.css"})
+    chrome.storage.sync.set({file: "styles/style-blue.css"})
 });
 
 purple.addEventListener("click", function () {
